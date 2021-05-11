28a6f5a2f0963de6b9f305b94b17d6c1ae2ef867
Duan Yutong <givoltage@gmail.com>
2021-05-11 11:28:22 -0400

    rewrite email module to use new EmailMessage class with utf-8 support

--- src/tweet_notifier/email.py
+++ src/tweet_notifier/email.py
@@ -4,6 +4,7 @@ import logging
 import os
 import smtplib
 import ssl
+from email.message import EmailMessage
 
 SUBJECT_TWEET_LIMIT = 140
 # Create a secure SSL context
@@ -11,10 +12,12 @@ SSL_CONTEXT = ssl.create_default_context()
 
 
 def get_smtp_config():
+    address = os.environ["SMTP_ADDRESS"]
     config = {
+        "address": address,
         "host": os.getenv("SMTP_HOST"),
         "port": os.getenv("SMTP_PORT"),
-        "username": os.getenv("SMTP_USERNAME"),
+        "username": address.split("@")[0],
         "password": os.getenv("SMTP_PASSWORD"),
     }
 
@@ -23,34 +26,53 @@ def get_smtp_config():
     return config
 
 
-def create_from_tweet(username, tweet):
+def create_from_tweet(username, tweet, address, receiver):
     """TODO: support different display timezones."""
-    if len(tweet["text"]) < SUBJECT_TWEET_LIMIT:
-        digest = tweet["text"]
-    else:
-        digest = f"{tweet['text'][:SUBJECT_TWEET_LIMIT]}..."
-    msg = f"""\
-    Subject: New tweet from @{username}: {digest}
-
+    digest = " ".join(tweet["text"].splitlines())
+    if len(tweet["text"]) > SUBJECT_TWEET_LIMIT:
+        digest = f"{digest[:SUBJECT_TWEET_LIMIT]}..."
+    msg = EmailMessage()
+    msg["Subject"] = f"New tweet from @{username}: {digest}"
+    msg["From"] = f"TweetNotifier <{address}>"
+    msg["To"] = receiver
+    content = f"""\
     @{username}: {tweet['text']}
+
+    ---
+
     https://twitter.com/{username}/status/{tweet['id']}
     {tweet['created_at']}
+
+    ---
+
     """ + (
-        "\nYou are receiving this email because "
+        "You are receiving this email because "
         "you are subscribed to the latest updates. "
         'To unsubscribe, reply with "unsubscribe".'
     )
-    return inspect.cleandoc(msg)
+    msg.set_content(inspect.cleandoc(content))
+    return msg
 
 
-def send_emails(msgs, receivers, host, port, username, password):
+def send_emails(
+    twitter_username,
+    tweets,
+    receivers,
+    address,
+    host,
+    port,
+    username,
+    password,
+):
     logger = logging.getLogger("TweetNotifier")
     logger.info("Authenticating SMTPs...")
     with smtplib.SMTP_SSL(host, port, context=SSL_CONTEXT) as server:
         server.login(username, password)
-        i, n_total = 0, len(receivers) * len(msgs)
-        for receiver, msg in itertools.product(receivers, msgs):
+        i, n_total = 0, len(receivers) * len(tweets)
+        for receiver, tweet in itertools.product(receivers, tweets):
+            # compose email on the fly
             i += 1
+            msg = create_from_tweet(twitter_username, tweet, address, receiver)
             logger.info(f"{i}/{n_total} sending email to {receiver}...")
-            server.sendmail(username, receiver, msg)
+            server.send_message(msg)
     logger.info(f"Done sending {n_total} emails")
