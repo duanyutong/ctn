d1739703c3ac3329e3d2ea898e819611e19dff35
Duan Yutong <givoltage@gmail.com>
2021-05-12 23:33:20 -0400

    more error handling for service unavailable response

--- src/tweet_notifier/apiv2.py
+++ src/tweet_notifier/apiv2.py
@@ -1,5 +1,8 @@
 """Lowest level functions that invoke twitter API v2."""
 
+import logging
+import time
+
 import requests
 
 # https://developer.twitter.com/en/docs/twitter-api/tweets/timelines/migrate
@@ -16,6 +19,12 @@ def connect_to_endpoint(bearer_token, url, params=None):
         "GET", url, headers=create_headers(bearer_token), params=params
     )
     if response.status_code != 200:
+        logger = logging.getLogger("TweetNotifier")
+        logger.error("Got error response: {response}")
+        if response.status_code == 503:  # Service Unavailable
+            logger.info("Service unavailable, waiting to retry...")
+            time.sleep(10)
+            return connect_to_endpoint(bearer_token, url, params=params)
         raise Exception(
             f"Request returned error {response.status_code}: {response.text}"
         )
